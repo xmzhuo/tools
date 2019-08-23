e8d15b268eb528be2bc9557f2a666eba1cb33945
xinming zhuo <zhuox@xinmings-MacBook-Pro.local>
2019-08-23 10:44:39 -0400

    add new tgrep to grep col by pattern at header

---
+++ tgrep.sh
@@ -0,0 +1,17 @@
+
+tgrep(){
+    #select by col_name
+    #tgrep $input $col_name $sep $grep_option
+    #selcol test.tsv .*_R , -v
+    line_num=$(sed -n 1p $1 | sed "s/${3}/\n/g" | grep $4 $2 -n | sed 's/\:.*$//g')
+    #echo $line_num
+    cat $1 | cut -f $(echo $line_num | sed 's/ /,/g') -d $3 
+    
+}
+
+file=$1
+pattern=$2
+sep=$3
+add_cmd=$4
+
+tgrep $file $pattern $sep $add_cmd 
